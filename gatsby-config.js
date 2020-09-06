8589ef654db8bbe954c592b9a983509721827018
takanakahiko <takanakahiko@gmail.com>
2020-09-07 02:50:50 +0900

    update siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `takanakahiko tech Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `takanakahiko`,
+      summary: `who lives and works in Tokyo building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `takanakahiko`,
     },
   },
   plugins: [
