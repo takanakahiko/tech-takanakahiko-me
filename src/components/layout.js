2f1de3a39ad28a9bb37a8d9027300df49f233e2d
takanakahiko <takanakahiko@gmail.com>
2020-09-07 03:54:51 +0900

    update Copyright notice

--- src/components/layout.js
+++ src/components/layout.js
@@ -59,7 +59,7 @@ const Layout = ({ location, title, children }) => {
       <header>{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        © takanakahiko {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.org">Gatsby</a>
       </footer>
